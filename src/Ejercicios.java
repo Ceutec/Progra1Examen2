5fb0925f27e31f7cc682e43c68ccd5afb5e00793
Turupawn <turupawn@userr>
2014-09-16 21:24:00 -0600

    primer commit

---
+++ src/Ejercicios.java
@@ -0,0 +1,58 @@
+
+public class Ejercicios {
+	//Devuelve el valor de "arreglo" en la posicion "i"
+	//2.5pts
+	static int obtenerValor(int arreglo[],int i)
+	{
+		return -1;
+	}
+	
+	//Si el promedio de todos los valores de "notas" es mayor a 60, devuelve verdadero de lo contrario falso
+	//2.5pts
+	static boolean alumnoAprobo(int notas[])
+	{
+		return false;
+	}
+	
+	//Devuelve el valor de mi_arreglo en la posicion [x][y]
+	//2.5pts
+	static int obtenerValorBidi(int mi_arreglo[][], int x, int y)
+	{
+		return -1;
+	}
+	
+	//Devuelve la cantidad de veces que "valor" existe en "arreglo"
+	//2.5pts
+	static int cuantosExisten(int arreglo[][], int valor)
+	{
+		return -1;
+	}
+	
+	//Realizar la siguiente funcion recursiva
+	//recursiva1(n) = recursiva1(n-1) - recursiva1(n-2)
+	//Dados los siguientes caso base
+	//recursiva1(0)=10
+	//recursiva1(1)=20
+	//2.5pts
+	static int recursiva1(int n)
+	{
+		return -1;
+	}
+	
+	//Realizar la siguiente funcion recursiva
+	//recursiva2(n) = recursiva2(n-1) + recursiva2(n-2) * recursiva2(n-3)
+	//Dados los siguientes caso base
+	//recursiva2(0)=10
+	//recursiva2(1)=20
+	//recursiva2(2)=30
+	//2.5pts
+	static int recursiva2(int n)
+	{
+		return -1;
+	}
+	
+	public static void main(String args[])
+	{
+		
+	}
+}
